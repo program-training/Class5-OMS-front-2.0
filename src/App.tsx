64243cc8a08212923a141980abec9215b47ceedf
shmuel cohen <shmuelicohen2000@gmail.com>
2023-12-05 10:49:57 +0200

    עשיתי את הקובץ apolloApi.ts עשיתי את הקובץ queries.ts והוספתי דברים לקובץ App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,38 @@ import { setToken } from "./features/token/tokenSlice";
 import { setLoading } from "./features/spinner/spinnerSlice";
 import { setLoggedUser } from "./features/users/usersSlice";
 import * as jwt from "jwt-decode";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { split, HttpLink } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:5000/graphql",
+});
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:5000/graphql",
+  })
+);
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   const token = useAppSelector((store) => store.token.token);
@@ -20,7 +52,7 @@ const App = () => {
   const isLogged = getToken();
   const RealToken = getRealToken();
   const dispatch = useAppDispatch();
-  console.log("logg:  " + isLogged, token);
+  console.log("log:  " + isLogged, token);
 
   if (loading) {
     dispatch(setToken(isLogged));
@@ -39,15 +71,17 @@ const App = () => {
 
   return (
     <>
-      <ThemeProvider theme={themeMode ? themeLight : themeDark}>
-        <CssBaseline />
-        {isLogged === "loggedin" || token === "loggedin" ? (
-          <HeaderLoggedIn />
-        ) : (
-          <Header />
-        )}
-        <RouterDOM />
-      </ThemeProvider>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={themeMode ? themeLight : themeDark}>
+          <CssBaseline />
+          {isLogged === "loggedin" || token === "loggedin" ? (
+            <HeaderLoggedIn />
+          ) : (
+            <Header />
+          )}
+          <RouterDOM />
+        </ThemeProvider>
+      </ApolloProvider>
     </>
   );
 };
--- src/features/orders/components/pages/OrdersTable.tsx
+++ src/features/orders/components/pages/OrdersTable.tsx
@@ -8,39 +8,46 @@ import {
   TablePagination,
 } from "@mui/material";
 import SearchField from "../ordersTable/SearchField";
-import { useAppDispatch, useAppSelector } from "../../../../store/hooks";
+// import { useAppDispatch, useAppSelector } from "../../../../store/hooks";
 import OrdersTableHead from "../ordersTable/OrdersTableHead";
 import OrdersBodyTable from "../ordersTable/ordersBodyTable/OrdersBodyTable";
 import useOrder from "../../hooks/useOrder";
 import { filteredOrdersUtils } from "../../../utils/utils";
-import getAllOrders from "../../service/getAllOrders";
+// import getAllOrders from "../../service/getAllOrders";
+import Order from "../../interfaces/order";
+import { useQuery } from "@apollo/client";
+import { GET_ORDERS } from "../../../recipes/services/apollo/queries";
 
 const OrdersTable = () => {
-  const { filteredOrders: orders } = useAppSelector((store) => store.orders);
-  const dispatch = useAppDispatch();
+  // const { filteredOrders: orders } = useAppSelector((store) => store.orders);
+  const [orders, setOrdersData] = useState<null | Order[]>(null);
+  const { loading, error, data } = useQuery(GET_ORDERS);
+  console.log(data.getAllOrdersFromMongoDB);
+
+  // const dispatch = useAppDispatch();
   useEffect(() => {
-    dispatch(getAllOrders());
+    data && setOrdersData(data.getAllOrdersFromMongoDB);
     console.log(orders);
-  }, []);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-  const { changeStatus } = useOrder(orders);
-  const { handleChangePage, handleChangeRowsPerPage, data } =
-    filteredOrdersUtils(
-      orders,
-      searchTerm,
-      page,
-      setPage,
-      rowsPerPage,
-      setRowsPerPage
-    );
+  }, [data, orders]);
 
-  useEffect(() => {
-    const timeoutId = setTimeout(changeStatus, 10000);
-    return () => clearTimeout(timeoutId);
-  }, [orders]);
+  if (loading) return <p>Loading... </p>;
+  if (error) return <p>{error.message}</p>;
 
+  // const [searchTerm, setSearchTerm] = useState("");
+  // const [page, setPage] = useState(0);
+  // const [rowsPerPage, setRowsPerPage] = useState(5);
+  // const { changeStatus } = useOrder(orders);
+  // const { handleChangePage, handleChangeRowsPerPage } = filteredOrdersUtils(
+  //   orders,
+  //   searchTerm,
+  //   page,
+  //   setPage,
+  //   rowsPerPage,
+  //   setRowsPerPage
+  // );
+
+  if (loading) return <p>Loading... </p>;
+  if (error) return <p>{error.message}</p>;
   return (
     <Container>
       <Box
